06efe3f21a55b049e54ea0d08dbb1c49bcd08bd3
Shay Dayan <Shay.Dayan@tufin.com>
2017-12-21 10:47:55 +0200

    Initial commit

---
+++ src/main/java/io/tufin/cloud/FirstTry/FirstTryApplication.java
@@ -0,0 +1,12 @@
+package io.tufin.cloud.FirstTry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTryApplication.class, args);
+	}
+}
